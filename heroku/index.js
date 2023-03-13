36fbb9856ba241ff1d764e38fb309e98f3c5489b
Abhiram Anand <75875359+AbhiramVAnand@users.noreply.github.com>
2023-03-13 21:50:46 +0530

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -55,7 +55,6 @@ app.post('/instagram', function(req, res) {
   console.log('Instagram request body:');
   console.log(req.body);
   // Process the Instagram updates here
-  console.log(res);
   received_updates.unshift(req.body);
   res.sendStatus(200);
 });
