d377b0d77900dd15007dff37ff17a439bd7c1943
Abhiram Anand <75875359+AbhiramVAnand@users.noreply.github.com>
2023-05-13 17:25:29 +0530

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -5,11 +5,11 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
-var xhub = require('express-x-hub');
+const bodyParser = require('body-parser');
+const express = require('express');
+const WebSocket = require('ws'); // Import the WebSocket library
+const app = express();
+const xhub = require('express-x-hub');
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
@@ -17,18 +17,55 @@ app.listen(app.get('port'));
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'token';
-var received_updates = [];
+const token = process.env.TOKEN || 'token';
+const received_updates = [];
+
+// Create a WebSocket server
+const wss = new WebSocket.Server({ noServer: true });
+
+// Handle WebSocket connections
+wss.on('connection', function connection(ws) {
+  console.log('WebSocket connected');
+  
+  // Handle WebSocket events here
+  
+  ws.on('message', function incoming(message) {
+    console.log('Received message:', message);
+    
+    // Process the WebSocket message here
+    
+    // Broadcast the message to all connected clients
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+    
+    // Invoke an action on your Android app here by sending the message to your app via a push notification or some other mechanism
+  });
+  
+  ws.on('close', function close() {
+    console.log('WebSocket closed');
+    
+    // Clean up resources or perform any necessary actions when a WebSocket connection is closed
+  });
+});
 
 app.get('/', function(req, res) {
   console.log(req);
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  
+  // Broadcast the changes to connected WebSocket clients
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(received_updates));
+    }
+  });
 });
 
 app.get('/init', function(req, res) {
   res.json({"Message": "Initialized"});
 });
-
 
 app.get(['/facebook', '/instagram'], function(req, res) {
   if (
@@ -62,6 +99,13 @@ app.post('/instagram', function(req, res) {
   // Process the Instagram updates here
   received_updates.unshift(req.body);
   res.sendStatus(200);
+});
+
+// Upgrade the initial HTTP request to a WebSocket connection
+app.on('upgrade', function upgrade(request, socket, head) {
+  wss.handleUpgrade(request, socket, head, function done(ws) {
+    wss.emit('connection', ws, request);
+  });
 });
 
 app.listen();
